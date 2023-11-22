023a261c8e97d4ba7bb240d2e94e0e1b132b34e3
Luca Bertaina <suhailpicciottobiz@gmail.com>
2023-11-22 09:25:13 -0300

    registro

--- js/registro.js
+++ js/registro.js
@@ -1,34 +1,31 @@
-var formulario = document.getElementsByClassName('cuestionario')[0]; // Asegúrate de seleccionar el primer formulario si hay varios
+
+var formulario = document.getElementsByClassName('cuestionario');
 var botonGuardar = document.getElementById('rgboton');
+var nombre = document.getElementById('nombre');
+var apellido = document.getElementById('apellido');
+var sexo = document.getElementById('sexo');
+var email = document.getElementById('email');
+var FDN = document.getElementById('FDN');
+var tell = document.getElementById('tell');
+var cuidad = document.getElementById('cuidad');
+var universidad = document.getElementById('Universidad');
+var documento = document.getElementById('documento');
+var constancia = document.getElementById('constancia');
+var user = document.getElementById('user');
+var contraeña = document.getElementById('contraseña');
 
-formulario.addEventListener('submit', function(event){
-    // Evitar que el formulario se envíe de manera predeterminada
-    event.preventDefault();
+botonGuardar.addEventListener('click', function() {
 
-    // Obtener los valores dentro del evento clic
-    var nombre = document.getElementById('nombre').value;
-    var apellido = document.getElementById('apellido').value;
-    var sexo = document.getElementById('sexo').value;
-    var email = document.getElementById('email').value;
-    var FDN = document.getElementById('FDN').value;
-    var tell = document.getElementById('tell').value;
-    var cuidad = document.getElementById('cuidad').value;
-    var universidad = document.getElementById('Universidad').value;
-    var documento = document.getElementById('documento').value;
-    var constancia = document.getElementById('constancia').value;
-    var user = document.getElementById('user').value;
-    var contrasena = document.getElementById('contraseña').value; // Corregir el nombre de la variable
-
-    console.log('Nombre:', nombre);
-    console.log('Apellido:', apellido);
-    console.log('Genero:', sexo);
-    console.log('Email:', email); // Corregir el nombre de la variable
-    console.log('Fecha de Naciomiento:', FDN);
-    console.log('Telefono celular:', tell);
-    console.log('Ciudadania:', cuidad);
-    console.log('Universidad:', universidad);
-    console.log('DNI/pasaporte:', documento);
-    console.log('Constancia:', constancia);
-    console.log('Usuario:', user);
-    console.log('Contraseña:', contraseña);
+    console.log('Nombre:', nombre.value);
+    console.log('Apellido:', apellido.value);
+    console.log('Genero:', sexo.value);
+    console.log('email:', email.value);
+    console.log('Fecha de Naciomiento:', FDN.value);
+    console.log('Telefono celular:', tell.value);
+    console.log('Ciudadania:', ciudad.value);
+    console.log('Universidad:', universidad.value);
+    console.log('DNI/pasaporte:', documento.value);
+    console.log('Constancia:', constancia.value);
+    console.log('Usuario:', user.value);
+    console.log('Contraseña:', contraeña.value);
 });
